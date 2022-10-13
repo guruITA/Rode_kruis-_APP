2ade6bbbd619cf70123762c0d496be8a3d5cb118
Gurpreet Singh <114655723+guruITA@users.noreply.github.com>
2022-10-13 09:23:29 +0200

    Na dat je op de button 'selecteer foto's kilkt krijg je de buttons volgende en vorige te zien'

--- app/src/main/java/com/example/rodekruisapp/FirstActivity.kt
+++ app/src/main/java/com/example/rodekruisapp/FirstActivity.kt
@@ -4,8 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -28,7 +30,6 @@ class FirstActivity : AppCompatActivity() {
     private var images: ArrayList<Uri?>? = null
     private var position = 0
     private val PICK_IMAGES_CODE = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,25 +60,35 @@ class FirstActivity : AppCompatActivity() {
 
         pickImageBtn.setOnClickListener {
             pickImageIntent()
+
+            if (previosBtn.isInvisible) {
+                previosBtn.setVisibility(View.VISIBLE)
+            } else {
+                previosBtn.setVisibility(View.INVISIBLE)
+            }
+
+            if (nextBtn.isInvisible) {
+                nextBtn.setVisibility(View.VISIBLE)
+            } else {
+                nextBtn.setVisibility(View.INVISIBLE)
+            }
         }
 
         nextBtn.setOnClickListener {
-            if (position < images!!.size-1) {
+            if (position < images!!.size - 1) {
                 position++
                 pickIImageSwitcher.setImageURI(images!![position])
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Geen foto's meer...", Toast.LENGTH_SHORT).show()
             }
         }
 
         previosBtn.setOnClickListener {
-            if (position > 0){
+            if (position > 0) {
                 position--
                 pickIImageSwitcher.setImageURI(images!![position])
-            }
-            else{
-                Toast.makeText(this,"Geen foto's meer...", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Geen foto's meer...", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -87,33 +98,30 @@ class FirstActivity : AppCompatActivity() {
         intent.type = "image/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select image(s)"  ), PICK_IMAGES_CODE)
+        startActivityForResult(Intent.createChooser(intent, "Select image(s)"), PICK_IMAGES_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == PICK_IMAGES_CODE){
+        if (requestCode == PICK_IMAGES_CODE) {
 
-            if (resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
 
-                if (data!!.clipData != null){
+                if (data!!.clipData != null) {
                     val count = data.clipData!!.itemCount
-                    for(i in 0 until count){
+                    for (i in 0 until count) {
                         val imageUri = data.clipData!!.getItemAt(i).uri
                         images!!.add(imageUri)
                     }
                     pickIImageSwitcher.setImageURI(images!![0])
                     position = 0
-                }
-                else{
+                } else {
                     val imageUri = data.data
                     pickIImageSwitcher.setImageURI(imageUri)
                     position = 0
                 }
             }
-
         }
     }
-
 }
